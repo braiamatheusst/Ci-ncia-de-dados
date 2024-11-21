d97f7a1f78e218507f09b72f86b978bd97f15c89
braia <108024615+braiamatheusst@users.noreply.github.com>
2024-11-21 16:55:00 -0300

    21/11

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -2,7 +2,16 @@ const url = 'https://raw.githubusercontent.com/braiamatheusst/Ci-ncia-de-dados/r
 
 async function visualiarInformacoesGlobais(){
     const res = await fetch(url);
+
     const dados = await res.json();
+
+    const paragrafo = document.createElement('p');
+    paragrafo.classList.add('graficos-container__texto');
+    paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
+    
+    const container = document.getElementById('graficos-container');
+    container.appendChild(paragrafo);
+    
 }
 
 visualiarInformacoesGlobais();
