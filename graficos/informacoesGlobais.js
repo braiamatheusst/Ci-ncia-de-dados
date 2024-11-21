fe46735d25bb4c97388d1aa7d939a9648c03e29c
braia <108024615+braiamatheusst@users.noreply.github.com>
2024-11-21 16:44:05 -0300

    21/11

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,1 +1,8 @@
-const url = 'https://raw.githubusercontent.com/TamirisCeolin/ciencia-de-dados-3C/refs/heads/main/dados-globais.json';
+const url = 'https://raw.githubusercontent.com/braiamatheusst/Ci-ncia-de-dados/refs/heads/main/dados-globais.json';
+
+async function visualiarInformacoesGlobais(){
+    const res = await fetch(url);
+    const dados = await res.json();
+}
+
+visualiarInformacoesGlobais();
